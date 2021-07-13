26a9ddc84356ca92a322ade7632683d167c8ea11
Leidy Ingrid <leidyingrid20@gmail.com>
2021-07-13 09:31:32 -0300

    First Commit

---
+++ src/main/java/ar/com/ada/api/adaairlines/AdaairlinesApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.adaairlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaairlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaairlinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/ada/api/adaairlines/AdaairlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.adaairlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaairlinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
